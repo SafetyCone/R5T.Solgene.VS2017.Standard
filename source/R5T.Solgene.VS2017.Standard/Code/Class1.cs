e4ec4f84dd5db3fec0f7947e570a153d0d569ee0
MinexAutomation <minex.automation@gmail.com>
2020-01-31 14:12:31 -0800

    Added IServiceCollection extensions.

--- source/R5T.Solgene.VS2017.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Solgene.VS2017.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Solgene.VS2017.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Solgene.VS2017.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioSolutionFileGenerator"/> service.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioSolutionFileGenerator(this IServiceCollection services)
+        {
+            services.AddVS2017VisualStudioSolutionFileGenerator();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioSolutionFileGenerator"/> service.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileGenerator> AddVisualStudioSolutionFileGeneratorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileGenerator>(() => services.AddVisualStudioSolutionFileGenerator());
+            return serviceAction;
+        }
+    }
+}
