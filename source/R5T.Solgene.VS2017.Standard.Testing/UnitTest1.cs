37e6f25e5d90a11f1643cdcff02ee69c78ea57db
MinexAutomation <minex.automation@gmail.com>
2020-01-31 14:01:40 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Solgene.VS2017.Standard.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Solgene.VS2017.Standard.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Solgene.VS2017.Standard/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Solgene.VS2017.Standard
+{
+    public class Class1
+    {
+    }
+}
