8d9857160aa52b026bc0c027f36291ca8108c125
MinexAutomation <minex.automation@gmail.com>
2020-02-01 19:18:01 -0800

    Added code to modify the SolutionFile instance to match the testing data VS2017 solution file (data changes, so ok!).

--- source/R5T.Solgene.VS2017.Standard.Construction/Code/Program.cs
+++ source/R5T.Solgene.VS2017.Standard.Construction/Code/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Bath;
 using R5T.Bedford;
+using R5T.Cambridge.Types;
 using R5T.Chalandri;
 using R5T.Evosmos;
 using R5T.Ilioupoli.Default;
@@ -53,8 +54,16 @@ namespace R5T.Solgene.VS2017.Standard.Construction
 
         public void Run()
         {
-            var newSolutionFile = this.VisualStudioSolutionFileGenerator.GenerateSolutionFile();
+            var newSolutionFile = this.VisualStudioSolutionFileGenerator.GenerateVisualStudioSolutionFile();
 
+            // Changes to make generated solution file match specific example solution file.
+            newSolutionFile.VisualStudioVersion = Version.Parse("15.0.28307.960");
+            newSolutionFile.MinimumVisualStudioVersion = Version.Parse("10.0.40219.1");
+
+            var extensibilityGlobals = newSolutionFile.GlobalSections.GetExtensibilityGlobalsGlobalSection();
+            extensibilityGlobals.SolutionGuid = Guid.Parse("67723507-8026-4C50-B327-62B320744C3E");
+
+            // Now serialize.
             var serializationFilePath = this.TemporaryDirectoryFilePathProvider.GetTemporaryDirectoryFilePath(TestingDataDirectoryContentConventions.NewVisualStudio2017SolutionFileNameValue);
 
             this.VisualStudioSolutionFileSerializer.Serialize(serializationFilePath, newSolutionFile);
